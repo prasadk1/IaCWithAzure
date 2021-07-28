dc5e8f040a4bc39281885d2b3b9f9049b66bd7ea
Prasad Kamat <prasadk1@gmail.com>
2021-07-28 12:56:20 -0400

    Update CreateTerraformBackend.sh

--- AutomationScripts/CreateTerraformBackend.sh
+++ AutomationScripts/CreateTerraformBackend.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #This script will create: an Azure resource group, an Storage account and a Storage container which will be used to store terraform state
 
-az group create --location eastus2 --name $TERRAFORM_RG
+az group create --location canadacentral --name $TERRAFORM_RG
 
-az storage account create --name $TERRAFORM_SA --resource-group $TERRAFORM_RG --location eastus2 --sku Standard_LRS
+az storage account create --name $TERRAFORM_SA --resource-group $TERRAFORM_RG --location canadacentral --sku Standard_LRS
 
-az storage container create --name terraform --account-name $TERRAFORM_SA
+az storage container create --name terrastgblob202107 --account-name $TERRAFORM_SA
